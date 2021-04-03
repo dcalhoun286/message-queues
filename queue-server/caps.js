e005705852ad8b8f35b9fe9a31acaed4911ee30e
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-04-03 16:59:07 -0700

    renamed main hub application file

--- queue-server/caps.js
+++ queue-server/caps.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
 
+const uuid = require('uuid').v4;
 const io = require('socket.io')(PORT);
+
+//remove messages from the list in the order they are received
+const queue = [];
 
 io.on('connection', (socket) => {
   console.log(`Client ${socket.id} has joined the server`);
